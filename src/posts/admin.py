8529d7f6ec3b7629ee5f50f86cd4dba46422a1ff
Younes Henni <henni1989younes@gmail.com>
2017-10-18 13:27:57 +0100

    Adding a custom admin panel and creating an basic Post model

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    '''
+    customizing the way infos about
+    our model post are displayed in the
+    Django admin
+    '''
+
+    list_display = (
+                    'user',
+                    'title',
+                    'slug',
+                    #'content',
+                    'updated',
+                    'created'
+                    )
+
+    list_display_links = ('user', 'updated')
+    list_editable = ['title'] #, 'content']
+    list_filter = ('updated', 'created')
+
+    '''
+    admin should be able to retreive a user post
+    by using one the following indicators
+    '''
+    search_fields = [
+                    'user__username',
+                    'user__first_name',
+                    'user__last_name',
+                    'slug',
+                    'title',
+                    'content'
+                    ]
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+
+class Post(models.Model):
+    '''
+    Users can post text and images to the news feed
+    '''
+    
+    # each post has a unique user (creator)
+    user = models.ForeignKey(User)
+    # this field is not required
+    title = models.CharField(max_length=120, blank=True)
+    content = models.TextField()
+    slug = models.SlugField(unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
